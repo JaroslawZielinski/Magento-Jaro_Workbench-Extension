f02f17e11eeb0ad9b1c0c5a800d284a37562f17d
Jarosław Zieliński <jzielinski@pandgroup.co>
2019-08-14 07:42:10 +0200

    first commit

---
+++ app/code/community/Workbench/PlainTable/Helper/Data.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * Class Workbench_PlainTable_Helper_Data
+ */
+class Workbench_PlainTable_Helper_Data extends Mage_Core_Helper_Abstract {
+
+}
---
+++ app/code/local/Jaro/MyScript/Helper/Data.php
@@ -0,0 +1,5 @@
+<?php
+ 
+class Jaro_MyScript_Helper_Data extends Mage_Core_Helper_Abstract {
+
+}
