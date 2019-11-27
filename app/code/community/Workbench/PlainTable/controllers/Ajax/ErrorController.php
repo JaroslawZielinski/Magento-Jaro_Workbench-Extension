19b5f188e93c61d377654fa66c9a7d872a601652
Jarosław Zieliński <jarek.zielinski82@gmail.com>
2019-11-27 07:09:40 +0100

    structural fix cd.

--- app/code/community/Workbench/PlainTable/controllers/Ajax/ErrorController.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-/**
- * Class Workbench_PlainTable_Ajax_ErrorController
- */
-class Workbench_PlainTable_Ajax_ErrorController extends Workbench_PlainTable_Controller_Ajax_AbstractController
-{
-
-}
